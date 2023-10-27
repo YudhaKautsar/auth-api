42ae4ce9c37cc12853a781d166b69f81c24ab38e
Yudha Kautsar <muhammad.kautsar@iconpln.co.id>
2023-10-27 10:20:19 +0700

    fixing tdd

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -43,7 +43,6 @@ const createServer = async (container) => {
       if (!translatedError.isServer) {
         return h.continue
       }
-
       // penanganan server error sesuai kebutuhan
       const newResponse = h.response({
         status: 'error',
